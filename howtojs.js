112290007a3fee9c2bbb5211f09cae46d7e64f12
Alberto Giunta <alb.giunta@gmail.com>
2015-07-07 01:27:06 +0200

    added support to research (only message passing)

---
+++ howtojs.js
@@ -0,0 +1,31 @@
+/* questo è per catturare il DOM dell'ext.
+tutto quello che deve apparire e succedere deve andare qua dentro
+*/
+document.addEventListener('DOMContentLoaded', function() {
+	// stuff
+});
+
+/*
+per ottenere un elemento del DOM
+*/
+var elem = document.getElementById("elemID");
+
+/*
+addEventListener
+*/
+elem.addEventListener("click", function() {
+	//stuff
+	// anonymous function
+});
+// OPPURE
+btn.addEventListener("click", nonAnonymousFunction); 
+function nonAnonymousFunction() {
+	// stuff
+}
+// per capire perchè non puoi usare nonAnonymousFunction()
+// senza parentesi (FUNCTION CALL): la funzione viene invocata nel document context
+// con parentesi (FUNCTION INVOCATION): la funzione viene invocata nel Window object context
+// inoltre il secondo parametro di addEventListener si aspetta un tipo funzione
+// e metterci le parentesi fa ritornare quello che ritorna la funzione (che non è a sua volta una funzione)
+// http://stackoverflow.com/questions/15255801/javascript-addeventlistener-function
+// http://stackoverflow.com/questions/593509/javascript-syntax-function-calls-and-using-parenthesis
--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-	
+    var loc = location.pathname.substring(1);
+
+    // se ci si trova sulla pagina principale (con ricerca serie tv)
+    if (loc == "popup.html") {
+	    var btn = document.getElementById("search-btn");
+	    btn.addEventListener("click", handler);
+	    document.getElementById("result").innerHTML = location.pathname.substring(1);
+		function handler() {			
+			var content = document.getElementById("search-tf").value;
+			document.getElementById("result").innerHTML = content;
+			localStorage.setItem("search-str", content);
+		}
+
+		// se ci si trova sulla pagina dei risultati di ricerca di una serie
+    } else if (loc == "result.html") {
+    	var str = localStorage.getItem("search-str");
+    	localStorage.removeItem("search-str");
+    	var lbl = document.getElementById("result");
+    	lbl.innerHTML = str;
+    }
 });
