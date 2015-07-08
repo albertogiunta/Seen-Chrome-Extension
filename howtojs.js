d0978e51f748f91ed17e7f331d3468fdaf2c8c55
Alberto Giunta <alb.giunta@gmail.com>
2015-07-08 23:15:57 +0200

    implemented save in chrome sync storage. listing of all users' series in main page TODO

--- howtojs.js
+++ howtojs.js
@@ -6,13 +6,97 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /*
-per ottenere un elemento del DOM
+html5 storage
 */
-var elem = document.getElementById("elemID");
+// viene trattato come un array associativo (stile php, una mappa)
+// http://diveintohtml5.info/storage.html
+localStorage.setItem("name", variable);
+localStorage.getItem("name");
+localStorage.removeItem("name");
 
 /*
-addEventListener
+accesso globale alle variabili
 */
+// se un js viene inserito in un html, se ha variabili globali queste possono essere semplicemente usate dagli altri file
+// altrimenti prova con window.variablename
+
+/*
+DOM manipulation
+*/
+document.getElementById("elementId");
+document.getElementById("elementId").setAttribute("attr", "value");
+document.getElementById("elementId").getAttribute("attr");
+document.getElementsByClassName("className");
+document.query...
+
+// elements creation
+var newEl = document.createElement("div");
+newEl.setAttribute("attr", "value");
+var node = document.createTextNode("insert text here");
+newEl.appendChild(node);
+
+/*
+CHROME STORAGE GET SET
+*/
+// clear
+chrome.storage.sync.clear();
+//---------------------------------------------------
+// set
+var save = keyForChromeStorage, 
+			jsonValuesForChromeStorage = JSON.stringify(
+														{'key': value, 
+														 'anotherkey': anothervalue});
+var jsonfile = {};
+jsonfile[save] = jsonValuesForChromeStorage;	
+chrome.storage.sync.set(jsonfile, function() {
+	console.log("save succeded");
+});
+//---------------------------------------------------
+//get NB è asincrono (se vuoi verificare che è andato a buon fine il set mettilo dentro il callback del set)
+chrome.storage.sync.get(keyForChromeStorage, function(obj) {
+	var plainJson = JSON.parse(obj[keyForChromeStorage]);
+	console.log(plainJson.key, plainJson.anotherkey):
+});
+
+// questo prende tutti i valori salvati nel chrome sync storage
+chrome.storage.sync.get(null, function(obj)) {
+	console.log(obj);
+});
+
+/*
+JSON manipulation
+*/
+var parsed = JSON.parse(plainJson);
+var plain = JSON.stringify({'key': value, "anotherkey": anothervalue});
+
+/*
+string manipulation
+*/
+string.substring(1);
+string.substring(0, 3);
+
+
+/*
+CALLBACK STUFF
+*/
+
+// i callback nascono per essere asincroni, quindi vengono eseguiti solo nel momento in cui 
+// quello che serve è pronto, non sono bloccanti
+// se vuoi eseguire un callback passandogli dei metodi il metodo più semplice è:
+document.someListener("stuff", function() {
+	someFunction(par, par, par);
+});
+
+function someFunction(par, par, par) {
+	// lot of stuff called from the callback
+};
+//---------------------------------------------------
+// per riferirti al contesto del callback usa il this
+document.someListener("stuff", function() {
+	console.log(this);
+})
+//---------------------------------------------------
+// addEventListener
 elem.addEventListener("click", function() {
 	//stuff
 	// anonymous function
@@ -29,34 +113,7 @@ function nonAnonymousFunction() {
 // e metterci le parentesi fa ritornare quello che ritorna la funzione (che non è a sua volta una funzione)
 // http://stackoverflow.com/questions/15255801/javascript-addeventlistener-function
 // http://stackoverflow.com/questions/593509/javascript-syntax-function-calls-and-using-parenthesis
-
-/*
-html5 storage
-*/
-// viene trattato come un array associativo (stile php, una mappa)
-// http://diveintohtml5.info/storage.html
-localStorage.setItem("name", variable);
-localStorage.getItem("name");
-localStorage.removeItem("name");
-
-/*
-accesso globale alle variabili
-*/
-// se un js viene inserito in un html, se ha variabili globali queste possono essere semplicemente usate dagli altri file
-// altrimenti prova con window.variablename
-
-/*
-cambiare valore attributo html
-*/
-document.getElementById("elementId").setAttribute("attr", "value");
-
-
-
-
-
-
-
-
+//---------------------------------------------------
 
 
 
--- popup.js
+++ popup.js
@@ -27,42 +27,104 @@ document.addEventListener('DOMContentLoaded', function() {
     } else if (loc == "result.html") {
         var str = localStorage.getItem("search-str");
         localStorage.removeItem("search-str");
+        // mostro i risultati per la stringa di ricerca
         if (str) {
             document.getElementById("title").innerHTML = "Results found for \"" + str + "\"";
             str = escape(str);
-            json = theMovieDb.search.getTv({"query":str}, successCB, errorCB);
+            json = theMovieDb.search.getTv({"query":str}, successSearch, errorSearch);
         } else {
             document.getElementById("title").innerHTML = "No results found!";
         }
     }
+    
+    var selectedName = null;
+    var selectedId = null;
 
-    function successCB(data) {
-    	console.log("Success callback: " + data);
+    // se la ricerca ha avuto successo creo un elenco puntato con i link di aggiunta di ogni possibile serie
+    function successSearch(data) {
+        // console.log("Success callback: " + data);
         result = JSON.parse(data);
-        console.log(result);
-        document.getElementById("result_list").appendChild(createList(createArray(result)));
+        document.getElementById("result_list").appendChild(createList(createArray(result))); // crea la lista
+        // aggiungo il listener per il click su ogni link
+        var cta = document.getElementsByClassName("tvs-a")
+        for (var i = 0; i < cta.length; i++) {
+            cta[i].addEventListener("click", function() {
+                // var regex = /.+?(?=\ \(([0-9])\w{3}\))/;
+                // selectedName = regex.test(tempName) ? regex.exec(tempName) : tempName;
+                console.log(this);
+                selectedName = this.getAttribute("data-tvsname");
+                selectedId = this.getAttribute("data-tvsid");
+                console.log(selectedId);
+                console.log(selectedName);
+                addTvs();
+            });
+        }
     };
 
-    function errorCB(data) {
+    // se la ricerca non ha avuto successo
+    function errorSearch(data) {
     	console.log("Error callback: " + data);
     }; 
 
+    // rimappo il json prendendo solo i valori che mi servono (id, nome, anno)
     function createArray(data) {
         var tvs = [];
         for (var i = 0; i < data.results.length; i++) {
-            tvs[i] = data.results[i].name;
+            tvs[i] = {};
+            tvs[i].id = "" + data.results[i].id;
+            tvs[i].name = data.results[i].name;
+            var date = data.results[i].first_air_date;
+            tvs[i].year = date ? " (" + date.substring(0,4) + ") " : "";
         }
         return tvs;
     }
 
+    // crea la lista html 
     function createList(data) {
         var list = document.createElement("ul");
+        list.setAttribute("id", "tvs-ul");
         for (var i = 0; i < data.length; i++) {
+            
             var item = document.createElement("li");
-            item.appendChild(document.createTextNode(data[i]));
+            item.setAttribute("class", "tvs-li")
+
+            var link = document.createElement("a");
+            link.setAttribute("class", "tvs-a");
+            link.setAttribute("data-tvsid", data[i].id);
+            link.setAttribute("data-tvsname", data[i].name);
+            link.setAttribute("href", "#");
+            var node = document.createTextNode("CTA");
+            link.appendChild(node);
+            
+            item.appendChild(document.createTextNode(data[i].name + data[i].year));
+            item.appendChild(link);
             list.appendChild(item);
         }
         return list;
+    }
+
+    function addTvs() {
+        theMovieDb.tvEpisodes.getById({"id":selectedId, "season_number": 1, "episode_number": 1}, successAdd, errorAdd)
+    }
+
+    function successAdd(data) {
+        result = JSON.parse(data);
+        var valueName = "next-" + selectedName + "-" + selectedId;
+        var save = valueName, selectedValues = JSON.stringify({'name':selectedName, 'id':selectedId});
+        var jsonfile = {};
+        jsonfile[save] = selectedValues;
+        // chrome.storage.sync.clear();
+        chrome.storage.sync.set(jsonfile, function() {
+            console.log("name saved as " + selectedName);
+            chrome.storage.sync.get(valueName, function(items) {
+            console.log(items);
+            });
+            // chrome.storage.sync.get(valueName, function(obj) {var a = JSON.parse(obj[valueName]); console.log("obj", a)});
+        });
+    }
+
+    function errorAdd(data) {
+
     }
 });
 
